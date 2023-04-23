5ac00d25dbf5eff66c13788eaa4ce7c1f4dc6a21
MSGronda <mgronda@itba.edu.ar>
2023-04-23 18:31:12 -0300

    feat: modified build script and removed .bat

--- script/build.sh
+++ script/build.sh
@@ -4,6 +4,11 @@ ROOT="$(dirname "$0")"
 
 echo "Relocating script to '$ROOT'..."
 cd "$ROOT"
+pwd
+
+echo "Removing previous verion..."
+rm ../src/frontend/lexical-analysis/flex-scanner.c ../src/frontend/syntactic-analysis/bison-parser.c ../src/frontend/syntactic-analysis/bison-parser.h
+rm -r ../bin
 
 echo "Building the compiler..."
 cmake -S .. -B ../bin
